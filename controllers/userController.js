cddff40136c94af1e909f4e3945f552ca4ced83e
Abeleshev <abeleshev1993@gmail.com>
2022-06-26 17:12:47 +0300

    add some functions of user contoller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,20 @@
 const ApiError = require('../error/apiError');
+const bcrypt = require('bcrypt');
+const {user, basket, basket} = require('../models/models');
 
 class UserContoller{//grououng function on class
-    async registration(req, res) {
-
+    async registration(req, res, next) {
+        const {email, password, role} = req.body;
+        if(!email || !password){
+            return next(ApiError.badRequest('missing data registration'));
+        }
+        const candidate = await user.findOne({where:{email}});
+        if(candidate){
+            return next(ApiError.badRequest('email is used'));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const itemuser = await user.create({email, role, password: hashPassword});
+        //const basket = await basket.create()
     }
     
     async login(req, res) {
@@ -12,7 +24,7 @@ class UserContoller{//grououng function on class
     async check(req, res, next) {
         const {id} = req.query;
         if(!id){
-            return next(ApiError.badRequest('missing id'))
+            return next(ApiError.badRequest('missing id'));
         }
         res.json(id);
     }
