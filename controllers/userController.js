a7d12713b129c20654adcb7956c896fee84d55dc
Abeleshev <abeleshev1993@gmail.com>
2022-06-27 18:02:38 +0300

    add work with jwt. Correct functions registration, login and check of userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,14 @@
 const ApiError = require('../error/apiError');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {user, basket, basket} = require('../models/models');
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign({id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'});
+
+}
 class UserContoller{//grououng function on class
     async registration(req, res, next) {
         const {email, password, role} = req.body;
@@ -13,20 +20,32 @@ class UserContoller{//grououng function on class
             return next(ApiError.badRequest('email is used'));
         }
         const hashPassword = await bcrypt.hash(password, 5);
-        const itemuser = await user.create({email, role, password: hashPassword});
-        //const basket = await basket.create()
+        const itemUser = await user.create({email, role, password: hashPassword});
+        const itemBasket = await basket.create({userId: itemUser.id});
+        const token = generateJwt(itemUser.id, itemUser.email, itemUser.role);
+
+        return res.json({token});
     }
     
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {email, password} = req.body;
+        const itemUser = await user.findOne({where:{email}});
+        if(!itemUser){
+            return next(ApiError.internal('user is undefined'));
+        }
+        let comparePassword = bcrypt.compareSync(password, itemUser.password);
+        if(!comparePassword){
+            return next(ApiError.internal('wrong password'));
+        }
+
+        const token = generateJwt(itemUser.id, itemUser.email, itemUser.role); 
+        return res.json({token});
+
     }
     
     async check(req, res, next) {
-        const {id} = req.query;
-        if(!id){
-            return next(ApiError.badRequest('missing id'));
-        }
-        res.json(id);
+        const token = generateJwt(req.user.id, req.user.email, req.user.role); 
+        return res.json({token});
     }
 }
 
